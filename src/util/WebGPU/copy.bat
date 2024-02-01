f9727243018219a13ba5b21068279cd11691944f
microaaron <microaaron@pku.edu.cn>
2024-02-02 04:14:10 +0800

    update

--- src/util/WebGPU/copy.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-for /f "delims=" %%i in ('dir /b *.js') do del "%%i"
-FOR %%A IN ("%~dp0.") DO set dst %%~dpA
-echo dst 
-for /d /r %%i in (*) do copy /y "%%i\*.js" dst
-::cd %~dp0
-for %%i in (*.js) do echo '%%i',
-copy /y .\src\WebGpu.js %~dp0
-::for /f "delims=" %%i in ('dir /b *.js') do  echo %%i,
