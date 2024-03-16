b1db86bb78e81c07485ab517d814a232a8f653ec
microaaron <microaaron@pku.edu.cn>
2024-03-17 07:03:47 +0800

    Update ShaderDynamic.js

--- src/shader/ShaderDynamic.js
+++ src/shader/ShaderDynamic.js
@@ -900,25 +900,25 @@ fn ${fragmentShaderModuleEntryPoint}(
     };
     
     
+    var alignOf=function (hostShareableType){
+      
+    }
     
-    var SizeOf=function (hostShareableType){
+    var sizeOf=function (hostShareableType){
       var match;
       switch (true) {
         case /^f16$/.test(hostShareableType):
           return 2;
           break;
         case /^i32$|^u32$|^f32$|^atomic<.*>$/.test(hostShareableType):
-        //case /^u32$/.test(hostShareableType):
-        //case /^f32$/.test(hostShareableType):
-        //case /^atomic<.*>$/.test(hostShareableType):
           return 4;
           break;
-        case (match=hostShareableType.match(/^vec(\d+)(.*)$/))?true:false:///^vec\d+<.*>$/.test(hostShareableType):
+        case (match=hostShareableType.match(/^vec(\d+)(.*)$/))?true:false:
           var N =Number(match[1]);//element count of the vector 
-          var T =match[2];//type
+          var T =match[2];//<type>
           switch (true) {
-            case (match=T.match(/^<(.*)>$/))?true:false:// /^<.*>$/.test(T):
-            return N*SizeOf(match[1]);
+            case (match=T.match(/^<(.*)>$/))?true:false:
+            return N*sizeOf(match[1]);
             break;
             case /^i$|^u$|^f$/.test(T):
             return N*4;
@@ -932,18 +932,17 @@ fn ${fragmentShaderModuleEntryPoint}(
         case (match=hostShareableType.match(/^mat(\d+)x(\d+)(.*)$/))?true:false:
           var C =Number(match[1]);//columns
           var R =Number(match[2]);//rows
-          var T =match[3];//type
-          return SizeOf(`array<vec${R}${T},${C}>`);
+          var T =match[3];//<type>
+          return sizeOf(`array<vec${R}${T},${C}>`);
           break;
         case (match=hostShareableType.match(/^array<(.*),(\d+)>$/))?true:false:
           var E =match[1];//element
           var N =Number(match[2]);//element count of the array
-          var sizeOfE=SizeOf(E);
-          var alignOfE=AlignOf(E);
+          var sizeOfE=sizeOf(E);
+          var alignOfE=alignOf(E);
           return N*(Math.ceil(sizeOfE/alignOfE)*alignOfE);
           break;
         default:
-          //console.log('404 Not Found');
       }
     }
     
